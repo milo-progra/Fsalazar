6f0ce1fe04fc231afe6bbdcf0d947d2ae6a8a1a4
fsalazar <fsalazar@hyl.cl>
2024-07-16 22:19:20 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { RouterLink,  } from 'vue-router'
+
+</script>
+
+<template>
+
+  <div class="h-screen">
+    <div class="flex justify-center items-center  h-[93%]">
+      <div class="rounded-lg w-5/6 h-5/6" style="background-color: rgba(0, 0, 0, 0.5);">
+        <RouterView/>
+      </div>
+    </div>
+    <div class="flex justify-center cursor-pointer">
+      <svg class="w-8 rotate-90" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="#ffffff" d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/></svg>
+    </div>
+  </div>
+  
+  
+</template>
+
+<style>
+body{
+  background-image: url('@/assets/img/fondo.jpg');
+  background-size: cover;
+  background-position: center;
+  height: 100vh; /* Ajusta según tus necesidades */
+  width: 100%;
+}
+
+h1, h2, p {
+  color: white;
+}
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,41 @@
+<script setup> 
+//Font  Fantasy
+</script>
+
+<template>
+    <div class="h-full w-full flex items-center justify-center">
+        <div class="w-full">
+            <div class="lg:grid lg:grid-cols-2 lg:gap-1 w-full ">
+                <div class="lg:mr-14">
+                    <div class="flex lg:justify-end justify-center">
+                        <img  src="../assets/img/yo.jpg" class="lg:w-2/4 w-3/4 rounded-full" alt="" srcset="">
+                    </div>
+                </div>
+                <div class="flex items-center mt-10 lg:mt-0">
+                    <div> 
+                        <div class="2xl:flex 2xl:justify-center">
+                            <img 
+                            src="../assets/img/sword.png" 
+                            class="w-20 "
+                            alt=""
+                            >
+                        </div>
+                        <h1 class="text-8xl">FABIÁN SALAZAR</h1>
+                        
+                        <h3 class="text-4xl">Desarrollador Software</h3>
+                        
+                        <p></p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<style>
+
+</style>
+
+
